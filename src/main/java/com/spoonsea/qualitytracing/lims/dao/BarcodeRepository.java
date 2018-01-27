14220611fb562558461a1c7014e828337791ed58
YUAN Mingyi <yuan.mingyi@wemarklinks.com>
2018-01-27 23:50:37 +0800

    modify a sql

--- src/main/java/com/spoonsea/qualitytracing/lims/dao/BarcodeRepository.java
+++ src/main/java/com/spoonsea/qualitytracing/lims/dao/BarcodeRepository.java
@@ -9,6 +9,7 @@ import com.spoonsea.qualitytracing.lims.model.Barcode;
 
 public interface BarcodeRepository extends JpaRepository<Barcode, Integer> {
 
+    @Query(value="select top(1) * from dbo.barcode where PackagingLine=?1 and date=?2 and time<?3 order by time desc", nativeQuery=true)
     List<Barcode> findTop1ByPackagingLineAndDateAndTimeLessThanOrderByTimeDesc(String packagingline, String date, String time);
 
     List<Barcode> findByBarcode(String barcode);
